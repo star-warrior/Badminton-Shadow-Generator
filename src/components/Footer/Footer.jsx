2e23d34197c4162af59fe3a32a5cd548a3d8d29d
Jay Mehta <69752445+star-warrior@users.noreply.github.com>
2025-08-11 11:08:42 +0530

    Footer Added

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Footer.css";
+
+import github from "../../assets/images/Github.png";
+import linkedin from "../../assets/images/linkedin.png";
+
+function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <div className="footer">
+      <hr />
+      <div className="footer-content">
+        <span>Made with ❤️, by Jay Mehta</span>
+
+        <p className="footer-copy">
+          &copy; {year} ShadowMate — All Rights Reserved
+        </p>
+        <div className="social-links">
+          <a href="https://github.com/star-warrior">
+            {" "}
+            <img src={github} alt="" />{" "}
+          </a>
+          <a href="www.linkedin.com/in/jay-mehta16">
+            <img src={linkedin} alt="" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
--- src/pages/Home-page/Homepage.jsx
+++ src/pages/Home-page/Homepage.jsx
@@ -4,25 +4,30 @@ import "./HomePage.css"; // Import the CSS file
 import Navbar from "../../components/Navbar/Navbar";
 import badminton_court from "../../assets/images/badminton_court.png";
 import shuttle from "../../assets/images/shuttlecock.png";
+import Footer from "../../components/Footer/Footer";
 
 const HomePage = ({ googleToken }) => {
   return (
-    <div className="home-container">
-      <Navbar on="homePage" googleToken={googleToken} />
-      <div className="hero-section">
-        <h1>Make your every step count</h1>
-        <Link to="/practice" className="practice-button">
-          Start Practice
-        </Link>
-        <img
-          className="court-image"
-          src={badminton_court}
-          alt="badminton court"
-        />
-        <img className="shuttle-image" src={shuttle} alt="shuttle" />
-        <img className="shuttle-image two" src={shuttle} alt="shuttle" />
+    <>
+      {" "}
+      <div className="home-container">
+        <Navbar on="homePage" googleToken={googleToken} />
+        <div className="hero-section">
+          <h1>Make your every step count</h1>
+          <Link to="/practice" className="practice-button">
+            Start Practice
+          </Link>
+          <img
+            className="court-image"
+            src={badminton_court}
+            alt="badminton court"
+          />
+          <img className="shuttle-image" src={shuttle} alt="shuttle" />
+          <img className="shuttle-image two" src={shuttle} alt="shuttle" />
+        </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 };
 
